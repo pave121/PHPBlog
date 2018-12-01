b68caaab9c06fa68bdbe9b9ea21ee4bcc83c66c7
Pavao Piršljin <pavaopirsljin@gmail.com>
2018-12-01 14:51:59 +0100

    started admin panel

---
+++ dashboard.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/adminstyles.css">
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2">
+            <h1>Pavao</h1>
+            <ul id="sideMenu" class="nav nav-pills nav-stacked">
+                <li class="active">
+                    <a href="dashboard.php">
+                    <span class="glyphicon glyphicon-th"></span>Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">
+                    <span class="glyphicon glyphicon-list-alt"></span>Add New Post</a>
+                </li>
+                <li>
+                   <a href="#">
+                    <span class="glyphicon glyphicon-tags"></span>Categories</a>
+                </li>
+                <li>
+                    <a href="#">
+                    <span class="glyphicon glyphicon-user"></span>Manage Admins</a>
+                </li>
+                <li>
+                <a href="#">
+                    <span class="glyphicon glyphicon-comment"></span>Comments</a>
+                </li>
+                <li>
+                    <a href="#">
+                    <span class="glyphicon glyphicon-equalizer"></span>Live Blog</a>
+                </li>
+                <li>
+                    <a href="#">
+                    <span class="glyphicon glyphicon-log-out"></span>Logout</a>
+                </li>
+            </ul>
+        </div> <!-- ending of side area -->
+        
+        <div class="col-sm-10">
+            <h1>Admin Dashboard</h1>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+            <h4>Sadrzaj</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quia corrupti aspernatur voluptas atque at minima, nulla est doloribus, dolores aut enim possimus saepe alias. Id labore quae voluptatum sunt?</p>
+        </div> <!-- ending of main area -->
+    </div> <!-- end of row -->
+</div> <!-- end of container -->
+
+<div id="footer">
+    <hr><p>Theme by: | Pavao Pirsljin &copy;2018. -- Bla bla bla</p>
+    <a style="color: white; text-decoration:none; cursor:pointer; font-weight:bold" href="http://www.google.hr">Google</a>
+</div>
+<div style="height:10px; background: #27aae1;"></div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
